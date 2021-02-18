515294897519fe3c202e99e908e1905f05835efd
Brian Chim <brianchim1@gmail.com>
2021-02-17 19:08:35 -0500

    made it so users who chose the correct horse get 1 point in startrace

--- src/main/java/com/thisisafakecom/thisisafakebot/commands/points/StartRaceCommand.java
+++ src/main/java/com/thisisafakecom/thisisafakebot/commands/points/StartRaceCommand.java
@@ -1,16 +1,22 @@
 package com.thisisafakecom.thisisafakebot.commands.points;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import com.thisisafakecom.thisisafakebot.App;
 import com.thisisafakecom.thisisafakebot.commands.CommandAbstract;
 import com.thisisafakecom.thisisafakebot.commands.IncorrectUsageException;
+import com.thisisafakecom.thisisafakebot.commands.points.database.PointsDBHandler;
 
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageReaction;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageUpdateEvent;
 
 public class StartRaceCommand extends CommandAbstract {
 
@@ -54,7 +60,7 @@ public class StartRaceCommand extends CommandAbstract {
 	    List<String[]> state = new ArrayList<String[]>();
 	    // init state
 	    state.add(horseTracks.clone());
-	    List<String> winners = new ArrayList<String>();
+	    List<Integer> winners = new ArrayList<Integer>();
 	    while (!winner) {
 	  	  // horse 1
 	  	  if (rng.nextDouble() < lowerBound) {
@@ -80,19 +86,19 @@ public class StartRaceCommand extends CommandAbstract {
 	  	  // horse won!
 	  	  if (horseAPos == 0) {
 	  		  winner = true;
-	  		  winners.add("Horse 1");
+	  		  winners.add(1);
 	  	  }
 	  	  if (horseBPos == 0) {
 	  		  winner = true;
-	  		  winners.add("Horse 2");
+	  		  winners.add(2);
 	  	  }
 	  	  if (horseCPos == 0) {
 	  		  winner = true;
-	  		  winners.add("Horse 3");
+	  		  winners.add(3);
 	  	  }
 	  	  if (horseDPos == 0) {
 	  		  winner = true;
-	  		  winners.add("Horse 4");
+	  		  winners.add(4);
 	  	  }
 	    }
 	  MessageChannel channel = input.getChannel();
@@ -103,8 +109,12 @@ public class StartRaceCommand extends CommandAbstract {
 			  							state.get(0)[2] + "\n" + state.get(0)[3] + "\n").complete();
 	  // add reactions to select the horse
 	  msgId = msg.getIdLong();
+	  List<String> winnerUnicodes = new ArrayList<String>();
 	  for (int k = 1; k <= 4; k++) {
 			String temp = "U+3" + k + "U+fe0fU+20e3";
+			if (winners.contains(k)) {
+				winnerUnicodes.add(temp);
+			}
 			msg.addReaction(temp).queue();
 	  }
 	  //:horse_racing:
@@ -114,16 +124,45 @@ public class StartRaceCommand extends CommandAbstract {
 				  					state.get(0)[0] + "\n" + state.get(0)[1] + "\n" + 
 				  					state.get(0)[2] + "\n" + state.get(0)[3] + "\n").queueAfter(countdown - i, TimeUnit.SECONDS);
 	  }
+	  // lock in the votes and get the winners
+	  Set<User> winningUsers = new HashSet<User>();
+	  Set<User> otherChoice = new HashSet<User>();
+	  App.waiter.waitForEvent(GuildMessageUpdateEvent.class, 
+			  e -> e.getMessage().getContentRaw().contains("The race will begin in 1 seconds.")
+			  && e.getMessage().getIdLong() == msgId,
+			  e -> {
+				  List<MessageReaction> reactions = channel.retrieveMessageById(msgId).complete().getReactions();
+				  for (MessageReaction r : reactions) {
+					  if (winnerUnicodes.contains(r.getReactionEmote().getAsCodepoints())) {
+						  for (User u : r.retrieveUsers().complete()) {
+							  if (winningUsers.contains(u)) {
+								  otherChoice.add(u);
+							  } else {
+								  winningUsers.add(u);								  
+							  }
+						  }
+					  } else {
+						  for (User u : r.retrieveUsers().complete()) {
+							  otherChoice.add(u);
+						  }
+					  }
+				  }
+				  // give the winners their points now
+				  winningUsers.removeAll(otherChoice);
+				  for (User u : winningUsers) {
+					  PointsDBHandler.addPoints(u, 1);
+				  }
+			  });
 	  // display the above simulation of the horse race
 	  for (int i = 0; i < state.size(); i++) {
 		  String[] currTrack = state.get(i);
-		  channel.editMessageById(msgId, "The horses are racing:\n"
+		  channel.editMessageById(msgId, "Votes have been locked in! The horses are racing:\n"
 				  					+ currTrack[0] + "\n" + currTrack[1] + "\n"
 				  					+ currTrack[2] + "\n" + currTrack[3] + "\n").queueAfter(countdown+i, TimeUnit.SECONDS);
 	  }
 	  String winnerMsg = "The winner(s) are: ";
 	  for (int i = 0; i < winners.size(); i++) {
-		  winnerMsg += winners.get(i) + ", ";
+		  winnerMsg += "Horse " + winners.get(i) + ", ";
 	  }
 	  winnerMsg = winnerMsg.substring(0, winnerMsg.length() - 2) + "!";
 	  channel.editMessageById(msgId, winnerMsg + "\n"
